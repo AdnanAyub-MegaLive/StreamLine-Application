74bb8b973ab264bb595d7ba4ed1a25fcdc415b95
obaid069 <obaidzafar234@gmail.com>
2026-07-16 17:59:04 +0500

    Add/update src/components/Screen.tsx

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,15 +1,29 @@
 import React, { PropsWithChildren } from 'react';
 import { StyleSheet, View, ViewStyle, StyleProp } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useTheme } from '../theme';
 
 type ScreenProps = PropsWithChildren<{
   style?: StyleProp<ViewStyle>;
+  transparent?: boolean;
 }>;
 
-export function Screen({ children, style }: ScreenProps) {
+export function Screen({ children, style, transparent }: ScreenProps) {
   const theme = useTheme();
+  const insets = useSafeAreaInsets();
 
-  return <View style={[styles.container, { backgroundColor: theme.surfaces.page }, style]}>{children}</View>;
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: transparent ? 'transparent' : theme.surfaces.page },
+        !transparent && { paddingTop: insets.top },
+        style,
+      ]}
+    >
+      {children}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
