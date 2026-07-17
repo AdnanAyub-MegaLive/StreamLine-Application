77d17e46822e999875e32aceb4231b8405a1c880
obaid069 <obaidzafar234@gmail.com>
2026-07-17 17:20:38 +0500

    Add/update src/api/client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { appEnv } from '../config/env';
 
 export const apiClient = axios.create({
-  baseURL: 'https://example.com/api',
+  baseURL: appEnv.apiBaseUrl,
   timeout: 15000,
 });
