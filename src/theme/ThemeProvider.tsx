79ccca1ae2859ee01a6bd2fb833098506ba5ac12
obaid069 <obaidzafar234@gmail.com>
2026-07-18 11:58:33 +0500

    Add/update src/theme/ThemeProvider.tsx

--- src/theme/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import ThemeContext from './ThemeContext';
-import { theme } from './theme';
-
-export function ThemeProvider({ children }: PropsWithChildren<{}>) {
-  return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
-}
-
-export default ThemeProvider;
