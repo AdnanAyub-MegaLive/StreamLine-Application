636e68f105d88e8a95f3efbb82e2504d02771cac
obaid069 <obaidzafar234@gmail.com>
2026-07-18 12:00:39 +0500

    Add/update src/assets/icons/SettingsIcon.jsx

---
+++ src/assets/icons/SettingsIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Svg, { Circle, Path } from 'react-native-svg';
+export function SettingsIcon({
+  size = 22,
+  color = '#7A8C89'
+}) {
+  return <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+      <Circle cx={12} cy={12} r={3} stroke={color} strokeWidth={2} />
+      <Path d="M12 3v2M12 19v2M4.2 6.2l1.4 1.4M18.4 16.4l1.4 1.4M3 12h2M19 12h2M4.2 17.8l1.4-1.4M18.4 7.6l1.4-1.4" stroke={color} strokeWidth={2} strokeLinecap="round" />
+    </Svg>;
+}
+export default SettingsIcon;
