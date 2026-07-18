00dae74cc0d7b82ed85223e29852b3174b3c183b
obaid069 <obaidzafar234@gmail.com>
2026-07-18 11:55:32 +0500

    Add/update src/components/index.ts

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Screen';
-export * from './BrandMark';
-export * from './PrimaryButton';
-export * from './FormField';
-export * from './TermsCheckbox';
-export * from './VideoBackground';
-export * from './LocationGate';
