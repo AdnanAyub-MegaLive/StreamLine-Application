676eca71c8e1a211a3e9a7a09a3db50c442eb97e
obaid069 <obaidzafar234@gmail.com>
2026-07-18 12:02:08 +0500

    Add/update src/navigation/routes.js

---
+++ src/navigation/routes.js
@@ -0,0 +1,14 @@
+export const routes = {
+  splash: 'Splash',
+  home: 'Home',
+  auth: 'Auth',
+  phoneAuth: 'PhoneAuth',
+  signupDetails: 'SignupDetails',
+  onboarding: 'Onboarding',
+  terms: 'TermsAndConditions',
+  room: 'Room',
+  profile: 'Profile',
+  banned: 'Banned',
+  settings: 'Settings',
+  editProfile: 'EditProfile'
+};
