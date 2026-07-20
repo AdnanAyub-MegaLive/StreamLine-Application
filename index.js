1d18c5256a3c5add5af4bd45b9bba29a5d291de8
obaid069 <obaidzafar234@gmail.com>
2026-07-20 19:58:00 +0500

    Add/update index.js

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import { navigateToRoom } from './src/navigation/navigationRef';
+import { registerBackgroundLiveRoomHandler } from './src/utils/liveRoomNotifications';
+
+// Must be registered at module scope (not inside a component) so Android
+// can deliver a "return to live room" notification tap even if the app was
+// backgrounded or fully killed.
+registerBackgroundLiveRoomHandler(navigateToRoom);
 
 AppRegistry.registerComponent(appName, () => App);
