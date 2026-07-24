950003613a6b0843fa7d06e1f705e4c78b84adc3
obaid069 <obaidzafar234@gmail.com>
2026-07-24 19:24:42 +0500

    Add/update src/assets/index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,6 @@ export * from './icons';
 export const streamlineLogo = require('./images/streamline-logo.png');
 export const roomBackgroundImage = require('./images/background-image.png');
 export const backgroundVideo = require('./video/background_video.mp4');
+
+
+
