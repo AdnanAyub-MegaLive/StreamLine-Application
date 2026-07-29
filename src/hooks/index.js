f01068364a9787dee0d418a072ddd37da0ed5d16
obaid069 <obaidzafar234@gmail.com>
2026-07-29 10:37:46 +0500

    Add/update src/hooks/index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,5 @@
+export * from './useAssignedBadge';
+export * from './useAssignedFrame';
 export * from './useAssignedRoomBackground';
 export * from './useBannerAssets';
 export * from './useSessionGuard';
